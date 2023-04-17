ff38ff496df799fc33f0a6df325f126be19e0584
Aodxfaionn <aodxfaionn@yandex.ru>
2023-04-17 23:56:25 +0300

    all-style

--- js/script.js
+++ js/script.js
@@ -5,47 +5,6 @@ menu.addEventListener("click", function () {
   nav.classList.toggle("open");
 });
 
-// Скролл вверх
-const scrollBtn = document.getElementById("scroll-top-button");
-function scrollFunction() {
-  if (
-    document.body.scrollTop > 150 ||
-    document.documentElement.scrollTop > 150
-  ) {
-    scrollBtn.style.display = "flex";
-    scrollBtn.addEventListener("click", function () {
-      document.body.scrollTop = 0;
-    });
-  } else {
-    scrollBtn.style.display = "none";
-  }
-}
-window.addEventListener("scroll", scrollFunction);
-
-function topFunction() {
-  let scrollStep = -window.scrollY / (500 / 15),
-    scrollInterval = setInterval(function () {
-      if (window.scrollY != 0) {
-        window.scrollBy(0, scrollStep);
-      } else clearInterval(scrollInterval);
-    }, 15);
-}
-
-// При скролле наверх появляется хедер
-const headerMini = document.querySelector(".headerMini");
-let prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-  let currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    headerMini.style.top = "0";
-  }
-  if (prevScrollpos < 450) {
-    headerMini.style.top = "-80px";
-    scrollBlock.classList.remove("open");
-  }
-  prevScrollpos = currentScrollPos;
-};
-
 const clientList = document.querySelectorAll(".clients__list"),
   pag = document.querySelector(".pagination");
 
